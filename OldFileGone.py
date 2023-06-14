934d7be11f458178cd85343cf81086fa7ff40182
AmirTaha <54143523+AmirTahaMim@users.noreply.github.com>
2023-06-14 15:26:02 +0330

    Add files via upload

---
+++ OldFileGone.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+def main():
+
+    dir_path = r"C:\Users\Administrator\Desktop\YT"
+    hour = 3
+    loop_sleep_time = 1
+
+    delete_file(dir_path, hour)
+    time.sleep(hour_to_Second(loop_sleep_time))
+
+def delete_file(dir_path, hour):
+    time.sleep(3600)
+    for file_name in os.listdir(dir_path):
+
+        file_path = os.path.join(dir_path, file_name)
+
+        if os.path.isfile(file_path):
+
+            # get the current time ==> time.time() & creation_time ==> os.path.getctime(file_path)
+
+            time_difference = time.time() - os.path.getctime(file_path)
+
+            if time_difference > hour_to_Second(hour):
+                os.remove(file_path)
+                print(f"File {file_name} deleted.")
+            else:
+                print(f"File {file_name} is not older than 1 hour.")
+
+def hour_to_Second(hour):
+    return hour * 3600
+
+main()
